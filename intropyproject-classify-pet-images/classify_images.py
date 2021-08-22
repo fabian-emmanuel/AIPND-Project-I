8fd983bb9928c007b59c4bae9ee25b81365b2a3a
FABIAN EMMANUEL <emmanuel.fabian@decagon.dev>
2021-08-22 19:29:55 +0100

    code-refactor

--- intropyproject-classify-pet-images/classify_images.py
+++ intropyproject-classify-pet-images/classify_images.py
@@ -69,7 +69,7 @@ def classify_images(images_dir, results_dic, model):
     """
     for key in results_dic:
         model_label = classifier(images_dir+key, model)
-        model_label.lower().strip()
+        model_label = model_label.lower().strip()
         truth = results_dic[key][0]
         if truth in model_label:
             results_dic[key].extend((model_label, 1))
